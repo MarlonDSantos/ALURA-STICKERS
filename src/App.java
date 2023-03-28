f7033f63673c35f4a0b7ef1707e464acbaa4d790
MarlonDSantos <104214966+MarlonDSantos@users.noreply.github.com>
2023-03-28 16:30:32 -0300

    Configurando gerador Stickers

---
+++ src/App.java
@@ -0,0 +1,48 @@
+import java.io.InputStream;
+import java.net.URI;
+import java.net.URL;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+import java.util.List;
+import java.util.Map;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+
+
+        //Fazer uma conecxão HTTP e buscar 250 top filmes Imdb
+        //String url = "https://imdb-api.com/en/API/Top250Movies/k_0ojt0yvm";
+        String url = "https://raw.githubusercontent.com/alura-cursos/imersao-java-2-api/main/TopMovies.json";
+        URI endereco = URI.create(url);
+        var client = HttpClient.newHttpClient();
+        var request = HttpRequest.newBuilder(endereco).GET().build();
+        HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+        String body = response.body();
+
+        //Extrair só os dados que interessam(titulo, poster, classificação)
+        var parser = new JsonParser();
+        List<Map<String, String>> listaDeFilmes = parser.parse(body);
+
+        System.out.println(listaDeFilmes.size());
+
+
+        // exibir e manipular os dados
+        for (Map<String,String> filme : listaDeFilmes) {
+            String urlImage = filme.get("image");
+            String titulo = filme.get("title");
+
+            InputStream inputStream = new URL(urlImage).openStream();
+            String nomeArquivo = titulo + ".png";
+
+            var geradora = new GeradoraFigurinhas();
+            geradora.cria(inputStream,nomeArquivo);
+
+            System.out.println(filme.get(titulo));
+            System.out.println(filme.get("image"));
+            System.out.println(filme.get("imDbRating"));
+            System.out.println();
+        }
+    }
+}
