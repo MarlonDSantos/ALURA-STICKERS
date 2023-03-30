01dad4a16be7dcbf770ffd2476b6d6e129336f3e
MarlonDSantos <104214966+MarlonDSantos@users.noreply.github.com>
2023-03-30 14:51:21 -0300

    Add metodo Post

--- linguagens-api/src/main/java/br/com/alura/linguagens/api/controller/LinguagemController.java
+++ linguagens-api/src/main/java/br/com/alura/linguagens/api/controller/LinguagemController.java
@@ -4,6 +4,8 @@ import br.com.alura.linguagens.api.model.Linguagem;
 import br.com.alura.linguagens.api.repository.LinguagemRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -25,7 +27,12 @@ public class LinguagemController {
     @GetMapping("/linguagens")
     public List<Linguagem> obterLinguagens(){
         List<Linguagem> linguagens = repositorio.findAll();
-
         return  linguagens;
+    }
+
+    @PostMapping("/linguagens")
+    public Linguagem cadastrarLinguagem(@RequestBody Linguagem linguagem){
+        Linguagem linguagemSalva = repositorio.save(linguagem);
+        return linguagemSalva;
     }
 }
